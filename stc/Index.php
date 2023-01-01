26f99db735107cff3443d93b71b271c89e182118
邹景立 <zoujingli@qq.com>
2023-01-01 14:41:06 +0800

    修改静态资源注释

--- stc/Index.php
+++ stc/Index.php
@@ -1,8 +1,22 @@
 <?php
 
+// +----------------------------------------------------------------------
+// | Static Plugin for ThinkAdmin
+// +----------------------------------------------------------------------
+// | 版权所有 2014~2023 Anyon<zoujingli@qq.com>
+// +----------------------------------------------------------------------
+// | 官方网站: https://thinkadmin.top
+// +----------------------------------------------------------------------
+// | 免费声明 ( https://thinkadmin.top/disclaimer )
+// +----------------------------------------------------------------------
+// | gitee 代码仓库：https://gitee.com/zoujingli/think-plugs-static
+// +----------------------------------------------------------------------
+
 namespace app\index\controller;
 
-class Index extends \think\admin\Controller
+use think\admin\Controller;
+
+class Index extends Controller
 {
     public function index()
     {
