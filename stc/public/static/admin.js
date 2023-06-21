84e7b39d42f40abea58fb2ca442ffdce464906fb
邹景立 <zoujingli@qq.com>
2023-06-21 23:02:37 +0800

    优化 data-blur-number 参数处理

--- stc/public/static/admin.js
+++ stc/public/static/admin.js
@@ -856,11 +856,12 @@ $(function () {
 
     /*! 表单元素失去焦点时数字 */
     $.base.onEvent('blur', '[data-blur-number]', function () {
-        let dset = this.dataset, min = dset.valueMin, max = dset.valueMax;
-        let value = parseFloat(this.value) || 0, fiexd = parseInt(dset.blurNumber || 0);
-        if (typeof min !== 'undefined' && value < min) value = min;
-        if (typeof max !== 'undefined' && value > max) value = max;
-        this.value = parseFloat(value).toFixed(fiexd);
+        let set = this.dataset, value = parseFloat(this.value) || 0;
+        let min = typeof set.valueMin === 'undefined' ? this.min : set.valueMin;
+        let max = typeof set.valueMax === 'undefined' ? this.max : set.valueMax;
+        if (typeof min !== 'undefined' && value < min) value = parseFloat(min) || 0;
+        if (typeof max !== 'undefined' && value > max) value = parseFloat(max) || 0;
+        this.value = value.toFixed(parseInt(set.blurNumber) || 0);
     });
 
     /*! 表单元素失焦时提交 */
